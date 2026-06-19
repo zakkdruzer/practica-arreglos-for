c84df96a7f7472e3f117e37c4910fb460577bd66
zakkdruzer <zakkdruzer@gmail.com>
2026-06-18 23:22:18 -0300

    reto 1 completado

---
+++ reto-1.js
@@ -0,0 +1,17 @@
+console.log("%cReto 1 · El inventario del explorador", "font-weight: bold; font-color: green; font-size: 15px;");
+console.log("")
+
+let mochila = ["cuerda", "linterna", "mapa", "brújula"];
+
+// 1) Muestra el PRIMER objeto
+console.log("Primer objeto:", mochila[0]);
+
+// 2) Muestra el ÚLTIMO objeto
+console.log("Último objeto:", mochila[mochila.length - 1]);
+
+// 3) Muestra cuántos objetos hay
+console.log("Total:", mochila.length);
+
+// 4) Cambia "mapa" por "GPS" y muestra la mochila
+mochila[2] = "GPS";
+console.log('Mochila: [ "' + mochila.join('", "') + '" ]');
